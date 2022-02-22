ade593b9b7ac43286835edf946ef7bb52e4d7d84
Den081985 <pishnenkod@gmail.com>
2022-02-22 23:01:11 +0300

    добавлена html-разметка

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h2 className="todo-title">Todo List</h2>
+      <form className="todo-form">
+        <h3 className="form-title">What needs to be done?</h3>
+        <input type="text" id="todo-input" />
+        <button type="submit" className="todo-add">
+          Add Todo
+        </button>
+      </form>
+      <ul className="todo-list">
+        <li className="todo-item">
+          <span className="todo-span">
+            <input type="checkbox" className="todo-input" />
+            Eat
+          </span>
+          <button className="todo-remove">&times;</button>
+        </li>
+        <li className="todo-item">
+          <span className="todo-span">
+            <input type="checkbox" className="todo-input" />
+            Code
+          </span>
+          <button className="todo-remove">&times;</button>
+        </li>
+        <li className="todo-item">
+          <span className="todo-span">
+            <input type="checkbox" className="todo-input" />
+            Sleep
+          </span>
+          <button className="todo-remove">&times;</button>
+        </li>
+        <li className="todo-item">
+          <span className="todo-span">
+            <input type="checkbox" className="todo-input" />
+            Repeat
+          </span>
+          <button className="todo-remove">&times;</button>
+        </li>
+      </ul>
     </div>
   );
 }
