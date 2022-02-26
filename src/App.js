f78a484c97c09508c6e1741f8a00b01902a0ede2
Den081985 <pishnenkod@gmail.com>
2022-02-26 14:40:27 +0300

    добавлены структура копонентов и функции toggleTaskCompleted,deleteTask,addTask

--- src/App.js
+++ src/App.js
@@ -1,46 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
+import FilterButton from "./Components/FilterButton";
+import Form from "./Components/Form";
+import Todo from "./Components/Todo";
 
-function App() {
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+  //функция изменения состояния отметок чекбоксов
+  function toggleTaskCompleted(id) {
+    const updateTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updateTasks);
+  }
+  //функция удаления элемента из списка
+  function deleteTask(id) {
+    const removedTasks = tasks.filter((task) => task.id !== id);
+    setTasks(removedTasks);
+  }
+  //всегда должны передавать уникальный ключ всему, что вы визуализируете с помощью итерации
+  const taskList = tasks.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+    />
+  ));
+  //функция-реквизит для добавления задачи в список
+  //nanoid генерирует уникальный id
+  function addTask(name) {
+    const newTask = { id: "todo" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+  const taskNoun = taskList.length !== 1 ? "tasks" : "task";
+  const taskHead = `${taskList.length} ${taskNoun} remaining`;
   return (
     <div className="wrapper">
       <h2 className="todo-title">Todo List</h2>
-      <form className="todo-form">
-        <h3 className="form-title">What needs to be done?</h3>
-        <input type="text" id="todo-input" />
-        <button type="submit" className="todo-add">
-          Add Todo
-        </button>
-      </form>
-      <ul className="todo-list">
-        <li className="todo-item">
-          <span className="todo-span">
-            <input type="checkbox" className="todo-input" />
-            Eat
-          </span>
-          <button className="todo-remove">&times;</button>
-        </li>
-        <li className="todo-item">
-          <span className="todo-span">
-            <input type="checkbox" className="todo-input" />
-            Code
-          </span>
-          <button className="todo-remove">&times;</button>
-        </li>
-        <li className="todo-item">
-          <span className="todo-span">
-            <input type="checkbox" className="todo-input" />
-            Sleep
-          </span>
-          <button className="todo-remove">&times;</button>
-        </li>
-        <li className="todo-item">
-          <span className="todo-span">
-            <input type="checkbox" className="todo-input" />
-            Repeat
-          </span>
-          <button className="todo-remove">&times;</button>
-        </li>
-      </ul>
+      <h3 className="form-title">What needs to be done?</h3>
+      <Form addTask={addTask} />
+      <div className="todo-filter">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h3 className="todo-head">{taskHead}</h3>
+      <ul className="todo-list">{taskList}</ul>
     </div>
   );
 }
